be74e7fe09dc3779683d58cbaa6d47a7bdaf075b
Bruno Rocha <bruno.miguel.rocha@hotmail.com>
2023-07-13 11:16:27 +0100

    updates july 23 students

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import "./App.css";
-import { useState } from "react";
+import './App.css';
+import { useState } from 'react';
 
 function App() {
   // Original list with all the students
-   let studentsList = [
-    "Ana R.",
-    "Bernardo A.",
-    "Cristian K.",
-    "Elnaz F.",
-    "Erik K.",
-    "Francisco B.",
-    "Francisco C.",
-    "Guglielmo G.",
-    "Guilherme D.",
-    "Gustavo C.",
-    "Guy D.",
-    "Henrique P.",
-    "João C.",
-    "João M.",
-    "Joie S.",
-    "Lucas T.",
-    "Margarida P.",
-    "Marisa P.",
-    "Niroj G.",
-    "Nuno D.",
-    "Pedro N.",
-    "Renato P.",
-    "Robson B.",
-    "Roshan P.",
-    "Tomás M.",
-    "Victoria A.",
-  ]; 
+  let studentsList = [
+    'Aamir M.',
+    'Agnes M.',
+    'Filipa F.',
+    'Paulina G.',
+    'André P.',
+    'Bruno C.',
+    'Gabriel O.',
+    'Gonçalo M.',
+    'J. Borrega',
+    'J. Elias',
+    'Julia G.',
+    'Manuel S.',
+    'Mariana M.',
+    'Mariana S.',
+    'Maureen T.',
+    'Mehdi N.',
+    'Miguel L.',
+    'Rita S.',
+    'Tiago R.',
+    'Tomás A.',
+  ];
 
   // Array of already picked students
   const [pickedStudents, setPickedStudents] = useState([]);
   // Array of pairs already assigned (results box)
   const [studentPairs, setStudentPairs] = useState([]);
   // Student that is picking a card
-  const [selectTarget, setSelectTarget] = useState("default");
+  const [selectTarget, setSelectTarget] = useState('default');
 
   // Get half of the cards
   const numOfCards = studentsList.length / 2;
@@ -48,45 +42,32 @@ function App() {
 
     // Check if the the picked card is valid
     if (
-      !e.target.classList.contains("flippedCard") &&
-      selectTarget !== "default" &&
+      !e.target.classList.contains('flippedCard') &&
+      selectTarget !== 'default' &&
       !pickedStudents.includes(selectTarget)
     ) {
       // Pick a random student and check if it is valid otherwise pick again
       for (let i = 0; i < 50; i++) {
-        randomStudent =
-          studentsList[Math.floor(Math.random() * studentsList.length)];
-        if (
-          randomStudent !== selectTarget &&
-          !pickedStudents.includes(randomStudent)
-        ) {
+        randomStudent = studentsList[Math.floor(Math.random() * studentsList.length)];
+        if (randomStudent !== selectTarget && !pickedStudents.includes(randomStudent)) {
           break;
         }
       }
 
       /* ------------------ Condition ------------------ */
       let sArray = [
-        "Cristian K.",
-        "Renato P.",
-        "Niroj G.",
-        "Joie S.",
         undefined,
         // don't leave this students to pick at the end
       ];
-      
-      let condition =
-        sArray.includes(randomStudent) && sArray.includes(selectTarget);
+
+      let condition = sArray.includes(randomStudent) && sArray.includes(selectTarget);
 
       if (condition) {
-        let rStudents = studentsList.filter(
-          (student) => !pickedStudents.includes(student)
-        );
+        let rStudents = studentsList.filter((student) => !pickedStudents.includes(student));
 
         do {
-          console.log(randomStudent)
-          let rIndex = Math.floor(
-            Math.random() * (studentsList.length - pickedStudents.length)
-          );
+          console.log(randomStudent);
+          let rIndex = Math.floor(Math.random() * (studentsList.length - pickedStudents.length));
           randomStudent = rStudents[rIndex];
         } while (sArray.includes(randomStudent));
       }
@@ -96,19 +77,16 @@ function App() {
       setPickedStudents([randomStudent, selectTarget, ...pickedStudents]);
 
       // Flip the card
-      e.target.classList.add("flippedCard");
+      e.target.classList.add('flippedCard');
       e.target.textContent = randomStudent;
 
       // Keep track of pairs already assigned
-      let emojis = ["🧠", "💻", "💪", "⚙️", "🤖", "⌨️", "💾", "📡", "📱"];
+      let emojis = ['🧠', '💻', '💪', '⚙️', '🤖', '⌨️', '💾', '📡', '📱'];
       let index = Math.floor(Math.random() * emojis.length);
-      setStudentPairs([
-        ...studentPairs,
-        `${selectTarget} ${emojis[index]} ${randomStudent}`,
-      ]);
+      setStudentPairs([...studentPairs, `${selectTarget} ${emojis[index]} ${randomStudent}`]);
 
       // Reset select dropdown to default after pair is assigned
-      setSelectTarget("default");
+      setSelectTarget('default');
     }
   };
 
@@ -156,8 +134,7 @@ function App() {
           </select>
 
           {/* ALERT MESSAGE IF STUDENT ALREADY HAS A PAIR */}
-          {pickedStudents.length > 1 &&
-          pickedStudents.includes(selectTarget) ? (
+          {pickedStudents.length > 1 && pickedStudents.includes(selectTarget) ? (
             <p id="selectAlert">Student already picked!</p>
           ) : (
             <p></p>
@@ -165,23 +142,17 @@ function App() {
         </div>
 
         {/* ALL CARDS WITH STUDENTS NAMES */}
-        <div className={"resultsAndCards"}>
+        <div className={'resultsAndCards'}>
           <div className="allCards">
             {studentsList.slice(0, numOfCards).map((student, index) => {
-              return (
-                <div
-                  key={index}
-                  className="card"
-                  onClick={(e) => pickFunction(e)}
-                ></div>
-              );
+              return <div key={index} className="card" onClick={(e) => pickFunction(e)}></div>;
             })}
           </div>
 
           {/* BOX WITH STUDENT PAIRS ALREADY ASSIGNED */}
           <div className="resultsBox">
             <p>Pairs:</p>
-            <ul id={"results"}>
+            <ul id={'results'}>
               {studentPairs.map((pair, index) => {
                 return <li key={index}>{pair}</li>;
               })}
